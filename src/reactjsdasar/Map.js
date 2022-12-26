fbde6fca6bbf036c6f8b3349b6f1b0bdc30ba128
syahrulawaludin <syahrulawaludin72@gmail.com>
2022-12-26 09:53:11 +0700

    map

--- src/reactjsdasar/Map.js
+++ src/reactjsdasar/Map.js
@@ -1,5 +1,7 @@
-import React from "react";
+ import React from "react";
 
+//  Step by step
+// 1. Membuat konstanta makanan
 const makanans = [
   {
     nama: "Bakso",
@@ -24,6 +26,9 @@ const makanans = [
 // Reduce : Mau menghitung total harga dari semua item
 
 
+
+// ========= Reduce ===================
+// 5. Membuat reduce, untuk menghitng total harga
 const total_bayar = makanans.reduce((total_harga,makanan) =>total_harga+makanan.harga, 0); 
 const Map = () => {
   return (
@@ -31,15 +36,21 @@ const Map = () => {
       <h2>Map Sederhana</h2>
       {/* Looping */}
       <ul>
+
+      {/* ===== Map Sederhana ========== */}
+      {/* 2. Membuat map agar data dapat ditampilkan */}
         {makanans.map((makanan, index) => (
           <li>
+          {/* 3. Menambahkan index dan ditambahkan 1. Agar index dimulai dari 1 dan tidak dari 0 */}
             {index + 1}. {makanan.nama} - Harga {makanan.harga}
           </li>
         ))}
       </ul>
+
+      {/* ========== Filter ============== */}
+      {/* 4.Membuat Filter */}
       <h2>Map Filter harga lebih dari 11.000</h2>
       <ul>
-
         {/* Difilter dulu baru ditampilkan */}
         {makanans
           .filter((makanan) => makanan.harga > 11000)
@@ -49,7 +60,7 @@ const Map = () => {
             </li>
           ))}
       </ul>
-
+            {/* 6. Memanggil variable total bayar, agar data array dapat ditampilkan */}
       <h3>Total harga : {total_bayar}</h3>
     </div>
   );
